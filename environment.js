04b0970d232c0fd25f528bd562049bcc7e9dc5c0
MomorioUHT <spirezaza.janthai000@gmail.com>
2026-07-14 15:58:45 +0700

    Encrypted

---
+++ environment.js
@@ -0,0 +1,5 @@
+// These are encrypted
+const ENV = {
+    ENCRYPTED_SHEET_ID: "U2FsdGVkX1/OgibtWBzvetSZBD2asxXIdRvk2rQ0WoT7brpCI5vRLWEzF7bdOCnthgoUCKeAT8zFUBf4XW/FSA==",
+    ENCRYPTED_SHEET_NAME: "U2FsdGVkX1+hMCIBlBavnyJvdpjX9nrxjwyF71jyoTw="
+};
